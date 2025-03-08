0b0cfb4cc6fccaff623b53e687988d21ed915347
Aryandeo8 <aryandeo@gmail.com>
2025-03-08 20:40:25 +0530

    debuggin and setting up the axios

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,16 +1,17 @@
-import {Router } from 'express';
+import { Router } from 'express';
 import {
-    loginUser,
-    registerUser,
-    refreshAccessToken,
-    getCurrentUser
+  loginUser,
+  registerUser,
+  refreshAccessToken,
+  getCurrentUser
 } from '../controllers/user.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
-const router = Router();
 
+const userRouter = Router();
 
-router.route("/login").post(loginUser);
-router.route("/refresh-token").post(refreshAccessToken);
-router.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/login").post(loginUser);
+userRouter.route("/register").post(registerUser); // Add a separate register route
+userRouter.route("/refresh-token").post(refreshAccessToken);
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
 
-export default router;
+export default userRouter;
