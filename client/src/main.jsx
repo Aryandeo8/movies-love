6252ccc669f4b9a8afc12d9bafb4a0231908b2d2
CoLH <153230337+Aryandeo8@users.noreply.github.com>
2025-03-17 12:22:15 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='540602665585-2bru74jrun5s1ttfbrjhlr2mompdrofu.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId='***'>
   <StrictMode>
     
     <BrowserRouter>
